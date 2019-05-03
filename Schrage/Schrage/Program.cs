38c0474b4df48210b67596eff239acf6ade28260
Kamil Kielbasa <kamilkielbasa73@gmail.com>
2019-05-03 16:31:07 +0200

    Added Schrange algorithm.
    - Added correct implementations.
    - Added tests which passed with values from lecturer.

---
+++ Schrage/Schrage/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrage
+{
+    public class RPQ
+    {
+        public int r { get; set; }
+        public int p { get; set; }
+        public int q { get; set; }
+
+        public RPQ(int r, int p, int q)
+        {
+            this.r = r;
+            this.p = p;
+            this.q = q;
+        }
+    }
+
+    public class FileReader
+    {
+        public string path { get; }
+
+        public FileReader(string pathToFile)
+        {
+            this.path = pathToFile;
+        }
+
+        public List<RPQ> Execute()
+        {
+            List<RPQ> listOfRPQ = new List<RPQ>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line = sr.ReadLine();
+                    string[] tokens = line.Split(null);
+
+                    tokens = tokens.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                    int numberOfRPQ = int.Parse(tokens[0]);
+
+                    for (int i = 0; i < numberOfRPQ; ++i)
+                    {
+                        line = sr.ReadLine();
+                        tokens = line.Split(null);
+                        tokens = tokens.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                        RPQ tmp = new RPQ(int.Parse(tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]));
+                        listOfRPQ.Add(tmp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return listOfRPQ;
+        }
+    }
+
+    public class SchrageAlgorithm
+    {
+        public static Tuple<RPQ[], int> Execute(List<RPQ> listOfRPQ)
+        {
+            int i = 0;
+            RPQ[] sigma = new RPQ[listOfRPQ.Count];
+            List<RPQ> listOfSortedRPQ = new List<RPQ>();
+            List<RPQ> listOfUnsortedRPQ = listOfRPQ;
+            int t = listOfUnsortedRPQ.Min(RPQ => RPQ.r);
+
+            int cmax = 0;
+
+            while (listOfSortedRPQ.Any() == true || listOfUnsortedRPQ.Any() == true)
+            {
+                while (listOfUnsortedRPQ.Any() == true && listOfUnsortedRPQ.Min(RPQ => RPQ.r) <= t)
+                {
+                    RPQ j = listOfUnsortedRPQ.Find(x => x.r == listOfUnsortedRPQ.Min(RPQ => RPQ.r));
+                    listOfSortedRPQ.Add(j);
+                    listOfUnsortedRPQ.Remove(j);
+                }
+
+                if (listOfSortedRPQ.Any() == false)
+                {
+                    t = listOfUnsortedRPQ.Min(RPQ => RPQ.r);
+                }
+                else
+                {
+                    RPQ j = listOfSortedRPQ.Find(x => x.q == listOfSortedRPQ.Max(RPQ => RPQ.q));
+                    listOfSortedRPQ.Remove(j);
+                    sigma[i] = j;
+                    i += 1;
+                    t += j.p;
+                    cmax = Math.Max(cmax, t + j.q);
+                }
+            }
+
+            return Tuple.Create(sigma, cmax);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Schrage/UnitTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Schrage;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodFor50RPQ()
+        {
+            string pathToTestData =
+              @"C:\Users\kielbkam\Desktop\Programming Platforms\discrete_processes\Schrage\Schrage\TestDate\in50.txt";
+
+            FileReader fr = new FileReader(pathToTestData);
+
+            List<RPQ> listOfRPQ = fr.Execute();
+
+            Tuple<RPQ[], int> output = SchrageAlgorithm.Execute(listOfRPQ);
+
+            RPQ[] listOfSortedRPQ = output.Item1;
+            int cmax = output.Item2;
+
+            Assert.AreEqual(1513, cmax);
+        }
+
+        [TestMethod]
+        public void TestMethodFor100RPQ()
+        {
+            string pathToTestData =
+              @"C:\Users\kielbkam\Desktop\Programming Platforms\discrete_processes\Schrage\Schrage\TestDate\in100.txt";
+
+            FileReader fr = new FileReader(pathToTestData);
+
+            List<RPQ> listOfRPQ = fr.Execute();
+
+            Tuple<RPQ[], int> output = SchrageAlgorithm.Execute(listOfRPQ);
+
+            RPQ[] listOfSortedRPQ = output.Item1;
+            int cmax = output.Item2;
+
+            Assert.AreEqual(3076, cmax);
+        }
+
+        [TestMethod]
+        public void TestMethodFor200RPQ()
+        {
+            string pathToTestData =
+              @"C:\Users\kielbkam\Desktop\Programming Platforms\discrete_processes\Schrage\Schrage\TestDate\in200.txt";
+
+            FileReader fr = new FileReader(pathToTestData);
+
+            List<RPQ> listOfRPQ = fr.Execute();
+
+            Tuple<RPQ[], int> output = SchrageAlgorithm.Execute(listOfRPQ);
+
+            RPQ[] listOfSortedRPQ = output.Item1;
+            int cmax = output.Item2;
+
+            Assert.AreEqual(6416, cmax);
+        }
+    }
+}
